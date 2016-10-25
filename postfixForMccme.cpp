61305b12af3fafe5f7ff0e537adbc0391fa12aba
betLomo <betta.po@yandex.ru>
2016-10-26 01:18:34 +0300

    Create postfixForMccme.cpp

---
+++ postfixForMccme.cpp
@@ -0,0 +1,118 @@
+/*Напишите программу, которая вычисляет значение арифметического выражения, записанно‐
+го в постфиксной форме. Выражение вводится с клавиатуры в виде символьной строки.*/
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+stringstream ss;
+
+int getNumbers(stack <string> &A, int &i, int &j)
+{
+    string tmp;
+    tmp=A.top();
+    A.pop();
+    i = atoi(tmp.c_str());
+
+    string tmp2;
+    tmp2 = A.top();
+    A.pop();
+    j =atoi(tmp2.c_str());
+
+    return i;
+    return j;
+}
+
+void inStack(stack <string> &A, int result)
+{
+    string send;
+    ss.clear();
+    ss<<result;
+    ss>>send;
+    A.push(send);
+}
+
+int main()
+{
+    stack <string> Stack;
+    string postfix;
+    string support;
+    string word;
+
+    getline (cin, postfix);
+
+    stringstream count(postfix),ss;
+
+    while(count>>support)
+    {
+        word=support;
+        if (word!="+"&&word!="-"&&word!="*"&&word!="/")
+            Stack.push (word);
+
+        else
+        {
+            if(word=="+")
+            {
+                int number1=0;
+                int number2=0;
+
+                getNumbers(Stack, number1, number2);
+
+                int end=number1+number2;
+
+                inStack (Stack, end);
+            }
+            if(word=="-")
+            {
+                int number1=0;
+                int number2=0;
+
+                getNumbers(Stack, number1, number2);
+
+                int end=number2-number1;
+
+                inStack (Stack, end);
+            }
+            if(word=="*")
+            {
+                int number1=0;
+                int number2=0;
+
+                getNumbers(Stack, number1, number2);
+
+                int end=number1*number2;
+
+                inStack (Stack, end);
+            }
+            if(word=="/")
+            {
+                int number1=0;
+                int number2=0;
+
+                getNumbers(Stack, number1, number2);
+
+                int end=number2/number1;
+
+                inStack (Stack, end);
+
+            }
+        }
+    }
+    string top;
+    top = Stack.top();
+    Stack.pop();
+    if (Stack.empty()==true)
+        cout<<top;
+    else cout << "ERROR";
+
+    return 0;
+}
+
